758fdda6dd01627247e8c3157561d6184a445fcd
reugn <reugpro@gmail.com>
2024-08-31 16:57:12 +0300

    test: add packer parallel test

--- packer_test.go
+++ packer_test.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"errors"
 	"strconv"
+	"strings"
 	"testing"
 
 	"github.com/reugn/tpack"
@@ -28,6 +29,27 @@ func TestPacker(t *testing.T) {
 
 	assertEqual(t, out.String(), "1\n2\n")
 	assertEqual(t, err.String(), "a\nb\nc\n")
+}
+
+func TestPacker_parallel(t *testing.T) {
+	in := bytes.NewBufferString("a\nb\n1\nc\n2")
+	var out bytes.Buffer
+	var err bytes.Buffer
+
+	tpack.NewPacker(in, &out, &err, tpack.NewProcessor(
+		func(str string) ([]string, error) {
+			var result []string
+			_, err := strconv.Atoi(str)
+			if err != nil {
+				return nil, errors.New(str)
+			}
+			return append(result, str), nil
+		},
+		tpack.Parallel(3),
+	)).Execute()
+
+	assertEqual(t, len(strings.ReplaceAll(out.String(), "\n", "")), 2)
+	assertEqual(t, len(strings.ReplaceAll(err.String(), "\n", "")), 3)
 }
 
 func assertEqual(t *testing.T, a, b any) {
